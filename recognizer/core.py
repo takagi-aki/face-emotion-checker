1b9c42d1e0bf616c397c0015329be4fa47e134ba
Akinari Takagi <102791616+takagi-aki@users.noreply.github.com>
2022-04-21 21:29:23 +0900

    顔認証インターフェース定義

---
+++ recognizer/core.py
@@ -0,0 +1,27 @@
+import os
+from typing import Callable, List, Tuple
+
+
+import cv2
+
+
+#
+# インターフェースクラス定義
+#
+
+
+class IRecognizer:
+
+    def register(self, name, img):
+        pass
+
+    def recognize(self, img: cv2.Mat):
+        pass
+
+
+#
+# インスタンス定義
+#
+
+
+# ここから下にクラスを追加する
