bc9e043929a63c9705edc6497dad09c02c3536cd
Akinari Takagi <102791616+takagi-aki@users.noreply.github.com>
2022-04-22 22:26:13 +0900

    カメラの読み込み解像度を指定可能にした

--- fec/camera.py
+++ fec/camera.py
@@ -6,8 +6,10 @@ import cv2
 
 class Camera:
 
-    def __init__(self) -> None:
+    def __init__(self, size=(720, 480)) -> None:
         self.cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+        self.cam.set(cv2.CAP_PROP_FRAME_WIDTH, size[0])
+        self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, size[1])
 
     @property
     def is_enable(self) -> bool:
